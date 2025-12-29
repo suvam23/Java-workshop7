5aaf30c6cdf61ba7efab1585820d93bcfbd8999c
SuvamPant <suvampant73@gmail.com>
2025-12-29 10:24:00 +0545

    grade calculator added

---
+++ GradeCalculator.java
@@ -0,0 +1,11 @@
+package workshop7;
+
+public class GradeCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Grade calculator");
+	}
+
+}
